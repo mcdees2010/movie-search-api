af91ebc8462dc135bc7cda910320e367c72e6f8b
Maria Dees <mcdees2010@gmail.com>
2018-09-28 22:33:30 -0700

    added middleware and apiclient const

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const express = require('express'),
       logger = require('morgan'),
       axios = require('axios');
 
+const apiClient = axios.create();
 
+// MIDDLEWARE
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.listen(process.env.PORT, err => {
     console.log(`running on port ${process.env.PORT}.....`);
