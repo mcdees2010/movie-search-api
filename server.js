f3b34d468d4085b904c6ea75d1307fba022f9fd7
Maria Dees <mcdees2010@gmail.com>
2018-09-29 10:28:39 -0700

    starting get route for search, checking params

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.get('/search/:term', (req, res) =>{
+    console.log(req.params.term);
+    // const apiUrl = `http://www.omdbapi.com/search/term?apikey=${process.env.OMDB_API_KEY}&`;
+    // apiClient({ method: "get", url: apiUrl}).then(apiRes => {
+    //     console.log(apiRes);
+    // })
+})
+
 app.listen(process.env.PORT, err => {
     console.log(`running on port ${process.env.PORT}.....`);
 })
