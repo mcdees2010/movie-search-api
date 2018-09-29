796192192b20ad8da92b26adc86c31bcaf2d052e
Maria Dees <mcdees2010@gmail.com>
2018-09-28 22:17:22 -0700

    added app.listen to listen to port 3000

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const express = require('express'),
       app = express(),
       logger = require('morgan'),
       axios = require('axios');
+
+
+
+app.listen(process.env.PORT, err => {
+    console.log(`running on port ${process.env.PORT}.....`);
+})
